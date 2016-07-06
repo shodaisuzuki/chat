9c5919da8232171d4b21579775ae349b4d817189
shodai <shodai-connect@tbz.t-com.ne.jp>
2016-07-06 22:58:09 +0900

    the entire function completion

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,41 @@
+ class BoardsController < ApplicationController
+  def index
+  	@boards = Board.all
+  end
+
+  def show
+  	@board = Board.find(params[:id])
+  end
+  
+  def new
+  	@board = Board.new
+  end
+   
+  def create
+  	@board = Board.new(params_board)
+  	@board.save
+  	redirect_to "/boards"
+   end
+
+   def edit
+  	@board = Board.find(params[:id])
+  end
+
+	def update
+  	@board = Board.find(params[:id])
+  	@board.update_attributes(params_board)
+  	redirect_to "/boards/#{@board.id}"
+  end
+
+  def destroy
+  	@board = Board.find(params[:id])
+  	@board.destroy
+  	redirect_to "/boards"
+  end
+
+   private
+
+   def params_board
+   	params.permit(:title, :editor)
+   end
+ end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Chat::Application.routes.draw do
+
+  get 'boards' => 'boards#index' 
+  get 'boards/new' => 'boards#new'
+  post 'boards' => 'boards#create'
+  get 'boards/:id' => 'boards#show'
+  get 'boards/:id/edit' => 'boards#edit'
+  put 'boards/:id' => 'boards#update'
+  delete 'boards/:id' => 'boards#destroy'
+  root "boards#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
